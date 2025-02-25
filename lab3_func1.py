82e88ae14d9e19fc5ed9b82480757acaad742c48
pusanc2 <pusanc2@illinois.edu>
2025-02-25 17:54:17 -0600

    Add files via upload

---
+++ lab3_func1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import numpy as np
+from scipy.linalg import expm
+from lab3_header import *
+
+"""
+Use 'expm' for matrix exponential.
+Angles are in radian, distance are in meters.
+"""
+
+def Get_MS():
+	# =================== Your code starts here ====================#
+	# Fill in the correct values for S1~6, as well as the M matrix
+	M = np.array([
+     [0, -1, 0, 0.39],
+     [0, 0, -1, 0.405],
+     [1, 0, 0, 0.215],
+     [0,0, 0, 1]
+		
+	])
+	
+	w1 = [0,0,1]
+	w2 = [0,1,0]
+	w3 = [0,1,0]
+	w4 = [0,1,0]
+	w5 = [1,0,0]
+	w6 = [0,1,0]
+ 
+	
+
+
+
+
+
+	# ==============================================================#
+	return M, S
+
+
+"""
+Function that calculates encoder numbers for each motor
+"""
+
+def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
+
+	# Initialize the return_value
+	return_value = [None, None, None, None, None, None]
+
+	# =========== Implement joint angle to encoder expressions here ===========
+	print("Foward kinematics calculated:\n")
+
+	# =================== Your code starts here ====================#
+
+
+
+
+
+
+
+
+
+	# ==============================================================#
+
+	print(str(T) + "\n")
+
+	return_value[0] = theta1 + PI
+	return_value[1] = theta2
+	return_value[2] = theta3
+	return_value[3] = theta4 - (0.5*PI)
+	return_value[4] = theta5
+	return_value[5] = theta6
+
+	return return_value
